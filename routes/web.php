33af20f3625e682104750e203f2d40f36713b1f7
SILAS JONATHAN <doyintechnology@gmail.com>
2026-07-22 11:36:42 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\DocumentController;
 | PUBLIC WEBSITE ROUTES
 |--------------------------------------------------------------------------
 */
-Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('./', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/services', [PageController::class, 'services'])->name('services');
 Route::get('/estates', [PageController::class, 'estates'])->name('estates');
